d6e8dca88aa80a1f9b58667e173a34163418b8a4
Radomir Polivka <radomir.polivka@gmail.com>
2026-03-02 23:38:14 +0000

    Refactor env.sh to define aliases directly instead of appending to bashrc

--- env.sh
+++ env.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-cat >> ~/.bashrc << 'EOF'
+# Git aliases
 alias gs='git status'
 alias gc='git commit -m '
 alias gb='git branch'
 alias gh='git push -u origin master:master'
 alias gl='git log --oneline -10'
 
+# History settings
 export HISTSIZE=10000
 export HISTFILESIZE=10000
-EOF
