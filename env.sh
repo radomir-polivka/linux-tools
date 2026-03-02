9249c0909dfb6ff7608c7e21904b121a8ef9924f
Radomir Polivka <radomir.polivka@gmail.com>
2026-03-02 23:36:16 +0000

    Update git log alias to show one-line per commit

--- env.sh
+++ env.sh
@@ -5,6 +5,7 @@ alias gs='git status'
 alias gc='git commit -m '
 alias gb='git branch'
 alias gh='git push -u origin master:master'
+alias gl='git log --oneline -10'
 
 export HISTSIZE=10000
 export HISTFILESIZE=10000
